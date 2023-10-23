f1301547e6ceb7c71411d827b66bd2b2e097216a
aimwill16 <williancosta16@gmail.com>
2023-10-23 17:54:05 -0300

    adicao do alerta do formulario

---
+++ script.js
@@ -0,0 +1,5 @@
+let formulario = document.getElementById("submit");
+
+formulario.addEventListener("click", ()=> {
+  alert("Pizza na Nuvem agradece seu contato. Responderemos em breve no seu email.")
+});
